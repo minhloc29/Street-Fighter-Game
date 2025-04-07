caac272b0fa242e692d62e24dcda97767e1b9f26
macbook <macbook@192.168.1.15.non-exists.ptr.local>
2025-04-07 13:47:22 +0700

    Add abstract state file

---
+++ src/character_state/Abstract.py
@@ -0,0 +1,12 @@
+class State:
+    def __init__(self, character: "Character"):
+        self.character = character
+    
+    def enter(self):
+        pass
+    
+    def exit(self):
+        pass
+    
+    def update(self):
+        pass
